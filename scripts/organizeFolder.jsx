8ef4784e7ac9f39370bdb3bac8ecc8e91779e1aa
housgreg <gregory.hamoui@orange.fr>
2023-01-13 12:37:09 +0100

    Update organizeFolder.jsx

--- scripts/organizeFolder.jsx
+++ scripts/organizeFolder.jsx
@@ -12,6 +12,34 @@ function organizeFolder() {
         items.push(app.project.item(i))
     }
 
+
+    //Ranger le dossier " Calques" 
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if (item instanceof FolderItem && item.name.endsWith(" Calques")) {
+            var hasAi = false;
+            var hasImages = false;
+            for (var j = 1; j <= item.numItems; j++) {
+                var subItem = item.item(j);
+                if (subItem instanceof FootageItem && subItem.file) {
+                    var file = new File(subItem.file.fsName);
+                    var extension = file.fsName.substring(file.fsName.lastIndexOf(".") + 1);
+                    if (extension == "ai") {
+                        hasAi = true;
+                    } else if (extension == "jpg" || extension == "png" || extension == "psd") {
+                        hasImages = true;
+                    }
+                }
+            }
+            if (hasAi && !hasImages) {
+                item.parentFolder = aiFolder;
+            } else if (!hasAi && hasImages) {
+                item.parentFolder = imageFolder;
+            }
+        }
+    }
+
+
     for (var i = 0; i <= items.length; i++) {
         var item = items[i];
         if (item instanceof FootageItem && item.file) {
@@ -31,18 +59,12 @@ function organizeFolder() {
         if (item instanceof CompItem && item.label != 9) {
             item.parentFolder = compsFolder;
         }
-        if (item instanceof CompItem && item.name.endsWith("- Comp 1")) {
+        var compRegExp = /- Comp \d+$/;
+        if (item instanceof CompItem && item.name.match(compRegExp)) {
             item.parentFolder = precompsFolder;
         }
     }
 
-    //Ranger le dossier " Calques" dans le dossier "ai"
-    for (var i = 0; i <= items.length; i++) {
-        var item = items[i];
-        if (item instanceof FolderItem && item.name.endsWith(" Calques")) {
-            item.parentFolder = aiFolder;
-        }
-    }
 
     function findFolder(folderName) {
         for (var i = 1; i <= app.project.numItems; i++) {
