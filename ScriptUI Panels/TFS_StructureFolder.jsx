2c8f2448b3fc29f00dd96271a77b418e934c1d11
housgreg <gregory.hamoui@orange.fr>
2023-01-23 17:35:50 +0100

    Update TFS_StructureFolder.jsx

--- ScriptUI Panels/TFS_StructureFolder.jsx
+++ ScriptUI Panels/TFS_StructureFolder.jsx
@@ -1,5 +1,5 @@
 //// Structure Folder AfterEffects /////
-//// Version Beta 0.6 - By Greg /////
+//// Version Beta 0.7 - By Greg /////
 
 ////// creer un fichier texte settings ////////
 var targetFolder = new Folder(Folder.userData.fullName +"/"+ "Aescripts"+"/"+"StructureFolder");
@@ -9,7 +9,7 @@ var defaults = [];
 var defaultFile = File(targetFolder.fullName + "/" + "Settings_StructureFolder.txt");
     if(!defaultFile.exists) {
         defaultFile.open("w");
-        defaultFile.write("src\rassets\rimages\rvideos\rai\raudios\rexports\relements\r1920\r1080\r120\rassets\rimages\rvideos\rai\raudios\rcomps\rpre-comps");
+        defaultFile.write("src\rassets\rimages\rvideos\rai\raudios/_preview\rexports\relements\r1920\r1080\r120\rassets\rimages\rvideos\rai\raudios\rcomps\rpre-comps");
         defaultFile.close();
     } else {
         defaultFile.open("r");
@@ -23,7 +23,7 @@ var defaultFile = File(targetFolder.fullName + "/" + "Settings_StructureFolder.t
 var panelGlobal = this;
 
 var uiWindow = (panelGlobal instanceof Panel) ? panelGlobal : new Window("palette", undefined, undefined, {resizeable: true}); 
-if ( !(panelGlobal instanceof Panel) ) uiWindow.text = "Structure Folder";
+if ( !(panelGlobal instanceof Panel) ) uiWindow.text = "StructureFolder";
 
 // GROUP1
 var group0 = uiWindow.add("group", undefined, {name: "group1"}); 
@@ -47,7 +47,8 @@ var groupLigne2 = group0.add("group", undefined, {name: "groupLigne2"});
     groupLigne2.margins = 0; 
 
 // Ajout champ de texte pour le nom du dossier
-var folderNameClient = groupLigne1.add("edittext", [0, 0, 160, 25], "0000_Client_Type");
+//var folderNameClient = groupLigne1.add("edittext", [0, 0, 160, 25], "0000_Client_Type");
+
 
 // Image boutons
 var imgSettings_imgString = "%C2%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%00%10%00%00%00%10%08%06%00%00%00%1F%C3%B3%C3%BFa%00%00%00%09pHYs%00%00%0B%13%00%00%0B%13%01%00%C2%9A%C2%9C%18%00%00%05%C3%A0iTXtXML%3Acom.adobe.xmp%00%00%00%00%00%3C%3Fxpacket%20begin%3D%22%C3%AF%C2%BB%C2%BF%22%20id%3D%22W5M0MpCehiHzreSzNTczkc9d%22%3F%3E%20%3Cx%3Axmpmeta%20xmlns%3Ax%3D%22adobe%3Ans%3Ameta%2F%22%20x%3Axmptk%3D%22Adobe%20XMP%20Core%209.0-c000%2079.171c27fab%2C%202022%2F08%2F16-22%3A35%3A41%20%20%20%20%20%20%20%20%22%3E%20%3Crdf%3ARDF%20xmlns%3Ardf%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%22%3E%20%3Crdf%3ADescription%20rdf%3Aabout%3D%22%22%20xmlns%3AxmpMM%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2Fmm%2F%22%20xmlns%3AstRef%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2FsType%2FResourceRef%23%22%20xmlns%3AstEvt%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2FsType%2FResourceEvent%23%22%20xmlns%3Axmp%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2F%22%20xmlns%3Adc%3D%22http%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%22%20xmlns%3Aphotoshop%3D%22http%3A%2F%2Fns.adobe.com%2Fphotoshop%2F1.0%2F%22%20xmpMM%3AOriginalDocumentID%3D%22xmp.did%3Aa5c64ca2-a541-4da7-94d8-0100b52f9b05%22%20xmpMM%3ADocumentID%3D%22xmp.did%3A0B79FB2FC55511EC95FECB1D6097022E%22%20xmpMM%3AInstanceID%3D%22xmp.iid%3A6dca8639-e01a-4ac6-b121-97b60d7351dc%22%20xmp%3ACreatorTool%3D%22Adobe%20Photoshop%2022.5%20(Macintosh)%22%20xmp%3ACreateDate%3D%222022-05-06T16%3A05%3A33%2B02%3A00%22%20xmp%3AModifyDate%3D%222023-01-11T19%3A09%3A35%2B01%3A00%22%20xmp%3AMetadataDate%3D%222023-01-11T19%3A09%3A35%2B01%3A00%22%20dc%3Aformat%3D%22image%2Fpng%22%20photoshop%3AColorMode%3D%223%22%3E%20%3CxmpMM%3ADerivedFrom%20stRef%3AinstanceID%3D%22xmp.iid%3Ace269a7f-025c-4fd0-97ed-26d81591ac86%22%20stRef%3AdocumentID%3D%22adobe%3Adocid%3Aphotoshop%3A63bc48f0-a9f9-b742-86ce-db79c04da5f5%22%2F%3E%20%3CxmpMM%3AHistory%3E%20%3Crdf%3ASeq%3E%20%3Crdf%3Ali%20stEvt%3Aaction%3D%22saved%22%20stEvt%3AinstanceID%3D%22xmp.iid%3A6dca8639-e01a-4ac6-b121-97b60d7351dc%22%20stEvt%3Awhen%3D%222023-01-11T19%3A09%3A35%2B01%3A00%22%20stEvt%3AsoftwareAgent%3D%22Adobe%20Photoshop%2024.1%20(Macintosh)%22%20stEvt%3Achanged%3D%22%2F%22%2F%3E%20%3C%2Frdf%3ASeq%3E%20%3C%2FxmpMM%3AHistory%3E%20%3C%2Frdf%3ADescription%3E%20%3C%2Frdf%3ARDF%3E%20%3C%2Fx%3Axmpmeta%3E%20%3C%3Fxpacket%20end%3D%22r%22%3F%3E%C2%88%09s4%00%00%018IDAT8%C2%8D%C2%8D%C2%93%C2%BBJCA%14EW%12%C2%8D%C2%92B%C3%A2%C2%A3%10%C3%93(%C2%88X%C2%99%C3%82B%7C%C2%80%20%C2%88%C2%B5%C3%B8%0D%C2%A2%C3%81%7FH'%04%C3%BC%07-%C2%AC%14k%0B%11%C3%BC%07%C2%B1%09%C2%BE%3A%C2%AD%C2%B4H%13T%C3%8C%C2%B20W%C3%87%C3%89%C2%BD%C3%A0%C2%86%0B%C3%83%C2%99%C3%8D%C3%99k%C3%A6%C3%8CE%25%C3%A3k%C3%BA%C2%ABJ%C2%96%2F%C3%8F%C2%B7r%40%038%00%26%C2%80-%60%C2%92_m%03%C3%A3%40%0D8%06%C3%8A%3F%3B%C3%9DN%C2%8D%20%C3%AD%C3%95t%C2%BD%04%C3%ABKu%20%24%C3%A8%0B%C3%92%C2%86I%C3%97H%C2%B0%C3%AE%07%0C%09%C3%8AQB%C2%A2N%06%C3%8Djx%07%05%60%0D%18%0A%12Z%C3%80%1EP%056%C2%81%C3%9B%C2%88f%1D(%25%04%C3%97%C3%AA%7B%C2%94%C2%B0%1B%C3%9D%C3%B6%C2%BC%C3%9A%C2%8E%3C%C3%B7%C3%AA%0E%19%C3%98%C3%93%C3%B6%C2%8E%C3%AC.%C3%85%7B%C2%96%07%C3%9A%11%5E%0E%C2%98%C2%8Dj%15%60%2C%C2%AAu%C2%80%26%C3%9D%C2%B4z%C3%94%C2%B9%C2%A9.%C2%A8%C2%83%C3%AA%C2%94z%1E%C3%AD%C2%9F%C2%A8U%C2%B5%C2%98%C3%A0%C2%95%C3%AC%C2%9D%C3%82%C2%87%C3%BA%C2%A0%C2%B6R%C3%90%17%C2%93%C2%A3%25%0Dj%C3%AAg%C2%8A1KGj%C3%81%C3%A0!%C2%AD%C3%80%C3%8F%C3%BA%3FZ%06%C2%8A%C3%A1C%1AU%2F%C3%94%2BuC%C3%9D%C2%8F%C2%88N%C3%95u%C3%B5P%C2%BDQ%C3%A7%C3%A2%23%C2%A0%C3%A6%C3%BD%3B%C2%B6%C3%A7%C2%A0%C3%81LP%1F%08%7D%C3%A1%3F%C3%90%C2%890%C3%AB%C3%80%12%C3%B0%04%3C%06%C3%B5%C2%B7%C3%90%C3%B4%05%C2%A1TH%C3%B44%C2%A3e%C3%BC%00%00%00%00IEND%C2%AEB%60%C2%82"; 
@@ -55,13 +56,15 @@ var imgCreate_imgString = "%C2%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%00%13%00%
 var imgOrganize_imgString = "%C2%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%00%1A%00%00%00%11%08%06%00%00%00%C3%83%C2%8D%C2%BC%0D%00%00%00%09pHYs%00%00%0B%13%00%00%0B%13%01%00%C2%9A%C2%9C%18%00%00%06%C3%81iTXtXML%3Acom.adobe.xmp%00%00%00%00%00%3C%3Fxpacket%20begin%3D%22%C3%AF%C2%BB%C2%BF%22%20id%3D%22W5M0MpCehiHzreSzNTczkc9d%22%3F%3E%20%3Cx%3Axmpmeta%20xmlns%3Ax%3D%22adobe%3Ans%3Ameta%2F%22%20x%3Axmptk%3D%22Adobe%20XMP%20Core%209.0-c000%2079.171c27fab%2C%202022%2F08%2F16-22%3A35%3A41%20%20%20%20%20%20%20%20%22%3E%20%3Crdf%3ARDF%20xmlns%3Ardf%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%22%3E%20%3Crdf%3ADescription%20rdf%3Aabout%3D%22%22%20xmlns%3AxmpMM%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2Fmm%2F%22%20xmlns%3AstRef%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2FsType%2FResourceRef%23%22%20xmlns%3AstEvt%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2FsType%2FResourceEvent%23%22%20xmlns%3Axmp%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2F%22%20xmlns%3Adc%3D%22http%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%22%20xmlns%3Aphotoshop%3D%22http%3A%2F%2Fns.adobe.com%2Fphotoshop%2F1.0%2F%22%20xmpMM%3AOriginalDocumentID%3D%22xmp.did%3Aa5c64ca2-a541-4da7-94d8-0100b52f9b05%22%20xmpMM%3ADocumentID%3D%22adobe%3Adocid%3Aphotoshop%3Aff63111d-6d28-f94a-8b3b-5ae1a201c080%22%20xmpMM%3AInstanceID%3D%22xmp.iid%3A15971f87-4bdf-4392-9336-2d0bb0996d93%22%20xmp%3ACreatorTool%3D%22Adobe%20Photoshop%2022.5%20(Macintosh)%22%20xmp%3ACreateDate%3D%222022-05-06T16%3A05%3A33%2B02%3A00%22%20xmp%3AModifyDate%3D%222023-01-14T14%3A12%3A03%2B01%3A00%22%20xmp%3AMetadataDate%3D%222023-01-14T14%3A12%3A03%2B01%3A00%22%20dc%3Aformat%3D%22image%2Fpng%22%20photoshop%3AColorMode%3D%223%22%3E%20%3CxmpMM%3ADerivedFrom%20stRef%3AinstanceID%3D%22xmp.iid%3Ace269a7f-025c-4fd0-97ed-26d81591ac86%22%20stRef%3AdocumentID%3D%22adobe%3Adocid%3Aphotoshop%3A63bc48f0-a9f9-b742-86ce-db79c04da5f5%22%2F%3E%20%3CxmpMM%3AHistory%3E%20%3Crdf%3ASeq%3E%20%3Crdf%3Ali%20stEvt%3Aaction%3D%22saved%22%20stEvt%3AinstanceID%3D%22xmp.iid%3A6dca8639-e01a-4ac6-b121-97b60d7351dc%22%20stEvt%3Awhen%3D%222023-01-11T19%3A09%3A35%2B01%3A00%22%20stEvt%3AsoftwareAgent%3D%22Adobe%20Photoshop%2024.1%20(Macintosh)%22%20stEvt%3Achanged%3D%22%2F%22%2F%3E%20%3Crdf%3Ali%20stEvt%3Aaction%3D%22saved%22%20stEvt%3AinstanceID%3D%22xmp.iid%3A15971f87-4bdf-4392-9336-2d0bb0996d93%22%20stEvt%3Awhen%3D%222023-01-14T14%3A12%3A03%2B01%3A00%22%20stEvt%3AsoftwareAgent%3D%22Adobe%20Photoshop%2024.1%20(Macintosh)%22%20stEvt%3Achanged%3D%22%2F%22%2F%3E%20%3C%2Frdf%3ASeq%3E%20%3C%2FxmpMM%3AHistory%3E%20%3C%2Frdf%3ADescription%3E%20%3C%2Frdf%3ARDF%3E%20%3C%2Fx%3Axmpmeta%3E%20%3C%3Fxpacket%20end%3D%22r%22%3F%3EV%3B%C2%9F%C3%BF%00%00%01%04IDAT8%C2%8D%C3%AD%C3%95%C2%BDJ%03A%14%05%C3%A0%2Fb%C2%A9%C2%9D%C2%AD%C3%81%3C%40%10%5BI%11Q%C2%B1%C3%B2%05%C3%84J%1B%7B%7B%25y%01%1B%5B%C3%B5%0D%2C%7C%01%05%25%C2%AD%C2%85%C2%8D%C2%85%10%C2%89%C2%A5%60%C2%93%07X%C2%8B%C2%9D%C3%85a%C3%98%C2%815%C2%A0U%0E%5Cv%C3%AE%C2%B9%C3%839s%C3%B7%C3%A7n%C2%AB%C3%B7ZHp%C2%8F~%C2%94O%C2%B1%C2%837%3C%C2%A3%1D%C3%95%26%C3%98%C3%80W*%C2%92b%C2%A1%C2%86%C3%AB'%C3%B926%C3%91ML%C2%84%C2%BC%C2%8Bu%7C%C2%A2%C2%88%C3%A2.%C3%9E%C2%B8%C2%989%C3%805%1EC%C3%BD2p%C2%ADp%3D%C3%83%07V1%0C%C3%BC%16V%12%C2%8D%C3%BD%26FG!%C3%AA0%C3%8C%C3%B0%C3%AF%C2%A1V%C3%A0%10%C3%9BM%C2%8C%C3%AA%3A%C2%AAp%C2%AE%7C6m%0C%22~%0D%17a%C2%BD%C2%94%0A%C3%8E%C3%92%C3%91%20%C3%83%C2%8Fq%C2%AA%C3%AC%C3%A8%04%7BM%C2%8CfA%07%C2%B7%C2%B9b%C3%9D%5B%C3%B7'%C2%98%1B%C3%8D%C2%8D%C3%BE%C3%9F(%C3%B7%1D%5D%C3%A1%C3%89%C3%AF%26%C3%83%C3%98%C3%8Fx%3A%C3%80n%13%C2%A3%C3%A3%101%C2%AA%C3%BFI%3A%19*%C2%BE%C2%83%C2%9B%C2%8C%5E%C3%AD%C2%AD%7BH%C3%B2)FxQv%12c%12%C3%B8Q%C3%98%C2%97%C3%95%C3%B9%06S%C2%AC3.%C2%83j%C2%BF%C2%B7%00%00%00%00IEND%C2%AEB%60%C2%82"; 
 var imgExport_imgString = "%C2%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%00%1C%00%00%00%10%08%06%00%00%00%05%C3%8F%1F%C3%AF%00%00%00%09pHYs%00%00%0B%13%00%00%0B%13%01%00%C2%9A%C2%9C%18%00%00%06%C3%81iTXtXML%3Acom.adobe.xmp%00%00%00%00%00%3C%3Fxpacket%20begin%3D%22%C3%AF%C2%BB%C2%BF%22%20id%3D%22W5M0MpCehiHzreSzNTczkc9d%22%3F%3E%20%3Cx%3Axmpmeta%20xmlns%3Ax%3D%22adobe%3Ans%3Ameta%2F%22%20x%3Axmptk%3D%22Adobe%20XMP%20Core%209.0-c000%2079.171c27fab%2C%202022%2F08%2F16-22%3A35%3A41%20%20%20%20%20%20%20%20%22%3E%20%3Crdf%3ARDF%20xmlns%3Ardf%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%22%3E%20%3Crdf%3ADescription%20rdf%3Aabout%3D%22%22%20xmlns%3AxmpMM%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2Fmm%2F%22%20xmlns%3AstRef%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2FsType%2FResourceRef%23%22%20xmlns%3AstEvt%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2FsType%2FResourceEvent%23%22%20xmlns%3Axmp%3D%22http%3A%2F%2Fns.adobe.com%2Fxap%2F1.0%2F%22%20xmlns%3Adc%3D%22http%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%22%20xmlns%3Aphotoshop%3D%22http%3A%2F%2Fns.adobe.com%2Fphotoshop%2F1.0%2F%22%20xmpMM%3AOriginalDocumentID%3D%22xmp.did%3Aa5c64ca2-a541-4da7-94d8-0100b52f9b05%22%20xmpMM%3ADocumentID%3D%22adobe%3Adocid%3Aphotoshop%3A6aecad24-830f-474a-8b15-bae1a7da679d%22%20xmpMM%3AInstanceID%3D%22xmp.iid%3Acc2ccd40-bf8b-42cd-8f05-9bae89548700%22%20xmp%3ACreatorTool%3D%22Adobe%20Photoshop%2022.5%20(Macintosh)%22%20xmp%3ACreateDate%3D%222022-05-06T16%3A05%3A33%2B02%3A00%22%20xmp%3AModifyDate%3D%222023-01-14T14%3A11%3A31%2B01%3A00%22%20xmp%3AMetadataDate%3D%222023-01-14T14%3A11%3A31%2B01%3A00%22%20dc%3Aformat%3D%22image%2Fpng%22%20photoshop%3AColorMode%3D%223%22%3E%20%3CxmpMM%3ADerivedFrom%20stRef%3AinstanceID%3D%22xmp.iid%3Ace269a7f-025c-4fd0-97ed-26d81591ac86%22%20stRef%3AdocumentID%3D%22adobe%3Adocid%3Aphotoshop%3A63bc48f0-a9f9-b742-86ce-db79c04da5f5%22%2F%3E%20%3CxmpMM%3AHistory%3E%20%3Crdf%3ASeq%3E%20%3Crdf%3Ali%20stEvt%3Aaction%3D%22saved%22%20stEvt%3AinstanceID%3D%22xmp.iid%3A6dca8639-e01a-4ac6-b121-97b60d7351dc%22%20stEvt%3Awhen%3D%222023-01-11T19%3A09%3A35%2B01%3A00%22%20stEvt%3AsoftwareAgent%3D%22Adobe%20Photoshop%2024.1%20(Macintosh)%22%20stEvt%3Achanged%3D%22%2F%22%2F%3E%20%3Crdf%3Ali%20stEvt%3Aaction%3D%22saved%22%20stEvt%3AinstanceID%3D%22xmp.iid%3Acc2ccd40-bf8b-42cd-8f05-9bae89548700%22%20stEvt%3Awhen%3D%222023-01-14T14%3A11%3A31%2B01%3A00%22%20stEvt%3AsoftwareAgent%3D%22Adobe%20Photoshop%2024.1%20(Macintosh)%22%20stEvt%3Achanged%3D%22%2F%22%2F%3E%20%3C%2Frdf%3ASeq%3E%20%3C%2FxmpMM%3AHistory%3E%20%3C%2Frdf%3ADescription%3E%20%3C%2Frdf%3ARDF%3E%20%3C%2Fx%3Axmpmeta%3E%20%3C%3Fxpacket%20end%3D%22r%22%3F%3E%C3%A6%01%C2%B3%C3%9A%00%00%00%C3%BDIDAT8%C2%8D%C3%8D%C3%94%C2%BF.DA%14%C3%87%C3%B1%C3%8F%C2%95%25!%12%C3%9BJ4%2B%12%C2%8D(D%C2%A3%C2%90%C3%A8%C2%BD%01%0F%C2%A0%C2%92%C2%88M%C2%88R%3C%C2%80R%C2%A9%C3%B4%18%1EB!%1A%C2%95B%C2%89%25(%C3%BC)vd%C3%87uB%C3%B6%C2%B27~%C3%89%C3%A4%C3%A4%C3%8C7s~%C2%999%C2%93S%C2%9C%C2%B6%C3%9Fd%C3%BA%C2%94%C3%BC%C2%B1%0A%18%0A%C3%80%01Z%C2%A5%C2%B5%C2%91%C3%98y%C3%80Z%C3%A8%24%C2%BE%1A%C2%B0%C3%A3%C2%BCx%230%7C%C3%84Mi%C3%AF!%C3%85%C2%97%C2%80%C3%91%7B%C2%99N%C3%80%C2%9F%C3%B3%C2%A4%C3%B8%0FO%C3%BA%C2%93%C2%AE~%C3%A3Z%C3%85p%13k%C2%B8%C2%AE%C3%8B%10N0%C2%8BC%C3%9D%C2%BE%0E%C3%9C%10%C3%AE%C2%B0%C2%8D%05%5C%C3%96a%08%C2%93%C3%98%C3%85%C3%B4%C2%A0%0D%1B%C3%98%C3%82%C2%85n%3F%C3%BB%3A%C3%98%C2%AF%C2%96%C2%B0%C2%8F%C3%B9%0Ag%2B%19%C3%AET1%C3%BA%C3%8E%C3%B0Ii%3A%60%18cx%C3%95%1Bc%C2%B9%26R%C2%BC%C3%B7%C3%B5%C3%97%C2%8Eb%C3%A4%23%C2%89z%C2%B8%C2%87fi%C2%AD'v%16%C2%B0%26n%13_%0E%C3%98Q%5E%3C%C2%BA%C3%A1%0CVJ%7Bs)%C2%8E%07%2C%C2%AF%C2%B3%C2%98LrM%C3%A5I%C3%AD%C2%B3%C3%B4%1D%03%C2%8D.%3E6pu%C2%A9%00%00%00%00IEND%C2%AEB%60%C2%82"; 
 
-var btnSettingsWindow = groupLigne1.add("iconbutton", undefined, File.decode(imgSettings_imgString), {name: "imgSettings", style: "toolbutton"});
-    btnSettingsWindow.helpTip = "Settings";
 
 var createButton = groupLigne2.add("iconbutton", undefined, File.decode(imgCreate_imgString), {name: "btnCreate", style: "toolbutton"});
-    createButton.text = "Create Folders"; 
-    createButton.helpTip = "Create Folders";
-    createButton.preferredSize.width = 110; 
+    createButton.text = "Create structure"; 
+    createButton.helpTip = "Create Folders & Ae";
+    createButton.preferredSize.width = 120; 
+
+var btnSettingsWindow = groupLigne2.add("iconbutton", undefined, File.decode(imgSettings_imgString), {name: "imgSettings", style: "toolbutton"});
+    btnSettingsWindow.helpTip = "Settings";
+    btnSettingsWindow.preferredSize.width = 40; 
 
 var divider0 = groupLigne2.add("panel", undefined, undefined, {name: "divider0"}); 
     divider0.alignment = "fill"; 
@@ -102,10 +105,14 @@ createButton.onClick = function() {
     }
 
     // Récupération du nom du dossier à partir du champ de texte
-    var folderName = folderNameClient.text;
+    var folderNameClient = prompt("Entrer le nom de dossier", "0000_Client_Type");
+    var folderName = folderNameClient; //ancienne methode folderNameClient.text
     
+    if (folderNameClient == null) {
+        return;
+    } else {
     // Ouvre une fenêtre de dialogue "Enregistrer sous" pour sélectionner un dossier
-    var saveFolder = Folder.selectDialog("Sélectionner le dossier de déstination");
+    var saveFolder = Folder.selectDialog("Selectionner le dossier de destination");
     
     // Si l"utilisateur a sélectionné un dossier, crée le dossier et enregistre le fichier After Effects
     if (saveFolder) {
@@ -205,6 +212,7 @@ createButton.onClick = function() {
     var projName = app.project.file.name.replace(/.aep/gi,"_1.0");
     var masterComp = app.project.items.addComp(projName, compW, compH, 1, defaults[10], 25);
     masterComp.label = 9; //couleur du calque
+    masterComp.name = masterComp.name.replace(/%20/g, " ").trim(); //pour avoir des espaces dans le nom de la composition
 
     //Creation des dossiers Ae
     var folderAssets = app.project.items.addFolder(defaults[11]);
@@ -235,14 +243,13 @@ createButton.onClick = function() {
 
     //Ouvrir la compo principale (masterComp)
     masterComp.openInViewer(); 
-
+    }
     /////////// After Effects //////////
 
-    } catch (e) {
-        // Affiche un message d"erreur si une erreur est survenue
-        alert("An error occurred: " + e.message);
-    }
-      
+        } catch (e) {
+            // Affiche un message d"erreur si une erreur est survenue
+            alert("An error occurred: " + e.message);
+        }
 };
 
 ///////////// fenetre settings //////////////
@@ -516,7 +523,7 @@ btnSettingsWindow.onClick = function () {
     ///////// Btn Reset et Save /////////
     resetButton.onClick = function() {
         defaultFile.open("w");
-        defaultFile.write("src\rassets\rimages\rvideos\rai\raudios\rexports\relements\r1920\r1080\r120\rassets\rimages\rvideos\rai\raudios\rcomps\rpre-comps");
+        defaultFile.write("src\rassets\rimages\rvideos\rai\raudios/_preview\rexports\relements\r1920\r1080\r120\rassets\rimages\rvideos\rai\raudios\rcomps\rpre-comps");
         defaultFile.open();
         settingsWindow.close();
     }
@@ -667,9 +674,9 @@ btnRenderExport.onClick = function () {
             BridgeTalk.launch("ame"); //pour lancer AME en arriere plan ("ame", "background")
             //alert("Ouverture de Media Encoder");
             }
-            var rqItem = app.project.renderQueue.items.add(comp);
+            var rqItem = app.project.renderQueue.items.add(comp); 
             var module = rqItem.outputModule(1);
-            module.file = File(path+"/"+comp.name.replace(".", "-"));
+            module.file = File(path+"/"+comp.name.replace(".", "-")); 
             app.project.renderQueue.queueInAME(true); 
             //module.file.parent.execute();
         }
